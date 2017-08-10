f40937459da6a084033b66fb3d29db5fc800b7cc
Smolya <mr.smolya@gmail.com>
2017-08-10 21:46:46 +0300

    add 6 task

---
+++ 6.js
@@ -0,0 +1,17 @@
+/*6
+Написать функцию, которая вернет новый объект, у которого ключи стали значениями, а значения ключами
+*/
+"use strict";
+
+function invert_key_value(obj) {
+    let newObj = {};
+
+    for (let key in obj) {
+        newObj[obj[key]] = key;
+    }
+
+    return newObj;
+}
+
+console.log( invert_key_value({red: "#FF0000", green: "#00FF00", white: "#FFFFFF"}) );
+// {#00FF00: "green", #FF0000: "red", #FFFFFF: "white"}
