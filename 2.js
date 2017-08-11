7417767096a780a5eef495c44e150fc684c5fe0f
Smolya <mr.smolya@gmail.com>
2017-08-12 00:45:54 +0300

    edit 3195 task

--- 2.js
+++ 2.js
@@ -2,12 +2,12 @@
 
 function randomNumber() {
     const MAX_TIME = 10000;        //max value of random interval
-    const START_TIME = Date.now();
-    const RAND_INTERVAL = Math.random()*MAX_TIME;
-    const END_TIME = START_TIME + RAND_INTERVAL;
+    const startTime = Date.now();
+    const randInterval = Math.random()*MAX_TIME;
+    const endTime = startTime + randInterval;
     let curTime = Date.now();
 
-    while( curTime < END_TIME ) {
+    while( curTime < endTime ) {
         curTime = Date.now();
     }
     return Math.random()*10;
