f9aff8a1fbad9b98ca056087af37c409e822b183
Smolya <mr.smolya@gmail.com>
2017-08-12 01:04:38 +0300

    edit 3589 task

--- 5.js
+++ 5.js
@@ -12,11 +12,11 @@ function range(start, stop, step = 1) {
         sequence.push(i);
     }
 
-    console.log(sequence);
+    return sequence;
 }
 
-range(10);              // => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
-range(1, 11);           // => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-range(0, 30, 5);        // => [0, 5, 10, 15, 20, 25]
-range(0, 10, 3);        // => [0, 3, 6, 9]
-range(0);               // => []
+console.log( range(10) );              // => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+console.log( range(1, 11) );           // => [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+console.log( range(0, 30, 5) );        // => [0, 5, 10, 15, 20, 25]
+console.log( range(0, 10, 3) );        // => [0, 3, 6, 9]
+console.log( range(0) );               // => []
