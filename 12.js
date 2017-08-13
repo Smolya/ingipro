689d2e07c3271e4f8e8ec93967778aac076f71ff
Smolya <mr.smolya@gmail.com>
2017-08-13 11:51:32 +0300

    edit 4532 task

--- 12.js
+++ 12.js
@@ -4,13 +4,27 @@ const str = 'Мама мыла раму';
 
 function truncate(string, lengthStr) {
     let outString = "";
+    let spaceFirstWord;
     let lengthCut;
 
-    lengthCut = string.lastIndexOf(" ",  lengthStr);
+    spaceFirstWord = string.indexOf(" ");
+
+    if(string.length <= lengthStr) {
+        return string;
+    }
+
+    lengthCut = (spaceFirstWord > lengthStr) ? spaceFirstWord : string.lastIndexOf(" ", lengthStr);
     outString = outString + string.substr(0, lengthCut) + "...";
 
-    console.log(outString);
+    return outString;
 }
 
-truncate(str, 7); // 'Мама...'
-truncate(str, 11); // 'Мама мыла...'
+console.log( truncate(str, 1) ); // 'Мама...'
+console.log( truncate(str, 11) ); // 'Мама мыла...'
+console.log( truncate(str, 0) ); // 'Мама...'
+console.log( truncate(str, 11) ); // 'Мама мыла...'
+console.log( truncate(str, 14) ); // "Мама мыла раму"
+console.log( truncate(str, 4) ); // "Мама..."
+console.log( truncate(str, 5) ); // 'Мама...'
+console.log( truncate(str, 9) ); // 'Мама мыла...'
+console.log( truncate(str, 15) ); // "Мама мыла раму"
