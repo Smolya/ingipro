d8156de6ec5c15da8282f27430fade2b29f19488
Smolya <mr.smolya@gmail.com>
2017-08-13 13:41:18 +0300

    2.3

--- 3.js
+++ 3.js
@@ -1,18 +1,19 @@
 /*3
-Написать функцию, котоая разбивает переданный массив arr на группы длиной size.
+Написать функцию, которая разбивает переданный массив arr на группы длиной size.
 */
 "use strict";
 
 function chunkArrayInGroups(arr, size) {
-    let copyArr = arr;
-    let outArr = [];
-    let numOfChunks = arr.length / size;
+    const copyArr = arr;
+    const outArr = [];
+    const lSize = (size === 0) ? arr.length : size;         //Если 0, то так же как и для arr.length
+    const numOfChunks = arr.length / lSize;
 
     for (let i = 0; i < numOfChunks; i++) {
-        outArr.push( copyArr.splice(0, size) );
+        outArr.push( copyArr.splice(0, lSize) );
     }
 
     return outArr;
 }
 
-console.log( chunkArrayInGroups(["a", "b", "c", "d"], 2) ); // [["a", "b"], ["c", "d"]]
+console.log( chunkArrayInGroups(["a", "b", "c", "d"], 0) ); // [["a", "b"], ["c", "d"]]
