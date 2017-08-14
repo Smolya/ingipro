66634203aca1fc5f5ae061f380646ec9d3567fe6
Smolya <mr.smolya@gmail.com>
2017-08-14 20:06:46 +0300

    3.1

--- 1.js
+++ 1.js
@@ -8,7 +8,7 @@
 
 "use strict";
 
-let town = prompt("Please, enter a city", "Moscow");
+const town = prompt("Please, enter a city", "Moscow");
 
 fetch(`http://api.openweathermap.org/data/2.5/weather?q=${town}&units=metric&appid=bd5e378503939ddaee76f12ad7a97608`)
     .then(function(response) {
