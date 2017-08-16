413b7f9f6e22ac86d09eff213b82152ab250c741
Smolya <mr.smolya@gmail.com>
2017-08-16 12:28:00 +0300

    3510

--- 3.js
+++ 3.js
@@ -1,10 +1,16 @@
 "use strict";
 
-const arr1 = [1, 2, 3, 4, 5];
+const arr1 = [1, 2, 2, 4, 5];
 const arr2 = [1, 5, 6, 7];
 
 function f(arr1, arr2) {
-    const mergerLine = arr1.slice();
+    const mergerLine = [];
+
+    for (let i = 0; i < arr1.length; i++) {
+        if (mergerLine.indexOf(arr1[i]) === -1) {
+            mergerLine.push(arr1[i]);
+        }
+    }
 
     for (let i = 0; i < arr2.length; i++) {
         if (mergerLine.indexOf(arr2[i]) === -1) {
