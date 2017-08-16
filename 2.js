dfaca314ff619a467375e9b07609c194dfaa5601
Smolya <mr.smolya@gmail.com>
2017-08-16 12:23:05 +0300

    3195

--- 2.js
+++ 2.js
@@ -5,11 +5,9 @@ function randomNumber() {
     const startTime = Date.now();
     const randInterval = Math.random()*MAX_TIME;
     const endTime = startTime + randInterval;
-    let curTime = Date.now();
 
-    while( curTime < endTime ) {
-        curTime = Date.now();
-    }
+    while( Date.now() < endTime ) { }
+    
     return Math.random()*10;
 }
 console.log(randomNumber());
