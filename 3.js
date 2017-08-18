3ed4b201f8b4ca3a9eb44d1f03512c8f032afdf3
Smolya <mr.smolya@gmail.com>
2017-08-18 21:26:15 +0300

    3.3

--- 3.js
+++ 3.js
@@ -12,19 +12,26 @@
 
 "use strict";
 
-const town = prompt("Please, enter a cities", "Moscow, London, New-York");
-const [...towns] = town.split(', ');
+function getTownsApi(towns) {
+    return towns.map( item => fetch(`http://api.openweathermap.org/data/2.5/weather?q=${item}
+									&units=metric&appid=bd5e378503939ddaee76f12ad7a97608`)
+        .then(response => response.json())
+.catch( error => console.error('Fetch error: ' + error.message) ) );
 
-const townsApi = towns.map( item => fetch(`http://api.openweathermap.org/data/2.5/weather?q=${item}
-												&units=metric&appid=bd5e378503939ddaee76f12ad7a97608`)
-    .then(response => response.json())
-.catch(alert)
-);
+}
 
-Promise.all(townsApi)
-    .then( function(cities) {
-        for (let city of cities) {
-            console.log(`${city.name}: температура ${city.main.temp}C, скорость ветра ${city.wind.speed}, влажность: ${city.main.humidity}%`);
-        };
-    })
-    .catch(alert);
+function printCitiesParam(cities) {
+    for (let city of cities) {
+        console.log(`${city.name}: температура ${city.main.temp}C, скорость ветра ${city.wind.speed}, влажность: ${city.main.humidity}%`);
+    };
+}
+
+function getWeather(cities) {
+    const [...towns] = cities.split(', ');
+
+    Promise.all(getTownsApi(towns))
+        .then( cities => printCitiesParam(cities) )
+        .catch( error => console.error('Fetch error: ' + error.message) );
+}
+
+getWeather('Moscow, London, New-York');
