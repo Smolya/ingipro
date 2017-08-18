245b78e74178e9040f3237bdc26cb9c54a5027ff
Smolya <mr.smolya@gmail.com>
2017-08-18 21:14:07 +0300

    3.2

--- 2.js
+++ 2.js
@@ -1,35 +1,54 @@
 "use strict";
 
-const town = prompt("Please, enter a city", "Moscow");
-
-fetch(`http://api.openweathermap.org/data/2.5/weather?q=${town}
-		&units=metric&appid=bd5e378503939ddaee76f12ad7a97608`)
-    .then(function(response) {
-        return response.json();
-    })
-    .then(function(town) {
-        return fetch(`http://api.openweathermap.org/data/2.5/find?
-					    lat=${town.coord.lat}&lon=${town.coord.lon}
-						&cnt=6&units=metric&appid=bd5e378503939ddaee76f12ad7a97608`);
-    })
-    .then(function(circle) {
-        return circle.json();
-    })
-    .then(function(towns) {
-        let maxInd = 0;
-        let max = towns.list[maxInd].main.temp;				//Текущий город
-
-        for (let i = 1; i < towns.list.length; i++) {
-            if ( towns.list[i].main.temp > max ) {
-                max = towns.list[i].main.temp;
-                maxInd = i;
+function getCityJsonParams(town) {
+    return fetch(`http://api.openweathermap.org/data/2.5/weather?q=${town}
+                &units=metric&appid=bd5e378503939ddaee76f12ad7a97608`)
+        .then(function(response) {
+            return response.json();
+        })
+        .then(function(params) {
+            return {
+                nums: 6,
+                lat: params.coord.lat,
+                lon: params.coord.lon
             }
+        })
+        .catch( error => console.error('Fetch error: ' + error.message) );
+}
+
+function getNearbyCitiesJsonParams(params) {
+    return fetch(`http://api.openweathermap.org/data/2.5/find?
+				lat=${params.lat}&lon=${params.lon}
+				&cnt=${params.nums}&units=metric&appid=bd5e378503939ddaee76f12ad7a97608`)
+        .then(function(circle) {
+            return circle.json();
+        })
+        .catch( error => console.error('Fetch error: ' + error.message) );
+}
+
+function getMaxTempCity(towns) {
+    let maxInd = 0;
+    let max = towns.list[maxInd].main.temp;				//Текущий город
+    for (let i = 1; i < towns.list.length; i++) {
+        if ( towns.list[i].main.temp > max ) {
+            max = towns.list[i].main.temp;
+            maxInd = i;
         }
-        (!maxInd) ?
-            alert(`В городе ${town} теплее, чем в 5 ближайщих городах`) :
-            alert(`В городе ${towns.list[maxInd].name} температура` +
-                ` ${towns.list[maxInd].main.temp}C. Это на` +
-                ` ${towns.list[maxInd].main.temp - towns.list[0].main.temp}C` +
-                ` теплее, чем в ${town}`);
-    })
-    .catch(alert);
+    }
+    (!maxInd) ?
+        alert(`В городе ${towns.list[0].name} теплее, чем в 5 ближайщих городах`) :
+        alert(`В городе ${towns.list[maxInd].name} температура` +
+            ` ${towns.list[maxInd].main.temp}C. Это на` +
+            ` ${towns.list[maxInd].main.temp - towns.list[0].main.temp}C` +
+            ` теплее, чем в ${towns.list[0].name}`);
+    console.log(towns);
+}
+
+function getWeather(town) {
+    getCityJsonParams(town)
+        .then( params => getNearbyCitiesJsonParams(params) )
+        .then( params => getMaxTempCity(params) )
+        .catch( error => console.error('Fetch error: ' + error.message) );
+}
+
+getWeather('Moscow');
